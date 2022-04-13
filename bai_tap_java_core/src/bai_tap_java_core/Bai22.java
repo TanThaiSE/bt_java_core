7b2f65e479df6feced7c77fcc8262a1f4c6cba14
TanThaiSE <tanthai172k@gmail.com>
2022-04-13 15:45:54 +0700

    add new assign

--- bai_tap_java_core/src/bai_tap_java_core/Bai22.java
+++ bai_tap_java_core/src/bai_tap_java_core/Bai22.java
@@ -1,10 +1,41 @@
 package bai_tap_java_core;
 
-public class Bai22 {
+import java.util.Scanner;
 
+public class Bai22 {
+	static int expandFromMiddle(String s,int left,int right) {
+		if(s==null||left>right) {
+			return 0;
+		}
+		while(left>=0&&right<s.length()&&s.charAt(left)==s.charAt(right)) {
+			left-=1;
+			right+=1;
+		}
+		return right-left+1;
+	}
+	static String longestSubStringPalindromic(String s) {
+		if(s==null||s.length()<0) {
+			return "";
+		}
+		int start=0;
+		int end=0;
+		for(int i=0;i<s.length();i++) {
+			int len1=expandFromMiddle(s, i, i);//th le
+			int len2=expandFromMiddle(s, i, i+1);//th chan
+			int len=Math.max(len1, len2);
+			if(len>end-start) {
+				start=i-((len-1)/2);
+				end=i+(len/2);
+			}	
+		}
+		return s.substring(start,end+1);	
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Nhap chuoi");
+		String s=sc.nextLine();
+		System.out.println(longestSubStringPalindromic(s));
 	}
 
 }
